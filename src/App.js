21a0e3721dff6008eb9e94ab01d9956e636cd9e0
thomasbulll <tombull66@hotmail.com>
2022-07-06 11:42:28 +0100

    eighth

--- src/App.js
+++ src/App.js
@@ -19,9 +19,11 @@ const App = () => {
       if (user) { 
         //load the contents of the users list from the database to the application
         onValue(ref(db, `/${auth.currentUser.uid}`), (snapshot) => {
-          //setting the list to an empty array
+          //Everytime the database is read it resets the array
           setList([]);
+          //The data from the database
           const data = snapshot.val();
+          //ensuring that there is data 
           if (data !== null) {
             Object.values(data).map((inputData) => {
               //populating the array with the input data
@@ -37,6 +39,7 @@ const App = () => {
 
 //using the remove operation to go into the users database and remove said item using it's ID
 const handleRemoveItem = (uidd) => {
+  // deleting from the database "db" using the user's uid and removing the product with the uidd
   remove(ref(db, `/${auth.currentUser.uid}/${uidd}`));
 }
 
@@ -49,17 +52,13 @@ const handleRemoveAll = () => {
   }
    
 }
-
-
 const SignInWithGoogle = () => {
   const provider = new GoogleAuthProvider();
   //using a popup to sign into google account
     signInWithPopup(auth, provider).then(result => {
         //storing the users name to be displayed on the page
         const name = result.user.displayName;
-        const photoURL = result.user.photoURL;
         localStorage.setItem("name", name);
-        localStorage.setItem("photoURL", photoURL);
         //catching and displaying errors 
     }).catch((error) => {
         alert(error);
@@ -101,24 +100,35 @@ const HandleKeyPress = (event) => {
 
 <div className="Header">
 
+{/* The title which has a google logo and the words "Shopping List" */}
 <h1 id="Title"><img id="GoogleLogo" alt="#" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"/> Shopping List</h1>
+{/* The image which allows users to sign into their google account */}
 <img id="GoogleSignIn" alt="#" src="https://www.oncrashreboot.com/images/create-apple-google-signin-buttons-quick-dirty-way-google.png" height="50" 
 width = "210" onClick={SignInWithGoogle}/>
+{/* The text which displays the users name when logged in */}
 <h3 id="UserName">Signed in as: {localStorage.getItem("name")}</h3>
 </div>
 
+
+{/* The button that removes every item from the list */}
 <input type = "button" id="RemoveAll" value="Delete Entire List" onClick={() => handleRemoveAll()} />
 
+
+{/* The div container which surronds the add feature and the displayed elements from the list */}
 <div className="EntireList">
 
 <div className = "input-field">
+  {/* The input field which allows users to add items to the list */}
   <input id="Add-Item" placeholder=" Add item" type="text" value={inputData} onKeyPress={HandleKeyPress} onChange = {(event) => setInputData(event.target.value)}/>
   
 </div>
 <div className="list-items">
+  {/* this maps the array into individual items */}
    {list.map((inputData) => {
     return (
       <div>
+       {/* each paragraph container contains an individual element from the array */}
+       {/* If the element is clicked it removes that item from the array and database */}
         <p id="item" onClick={() => handleRemoveItem(inputData.uidd)}>{inputData.inputData}</p>
         
       </div>
